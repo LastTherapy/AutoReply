1c8f24c4091c2bd4f04e447b00230579e1f183e7
hellomind <random1250@gmail.com>
2025-04-02 01:04:50 +0300

    fix

--- hh_auth/models.py
+++ hh_auth/models.py
@@ -7,7 +7,8 @@ class HHUser(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField(null=True, blank=True)
-    access_token = models.TextField()refresh_token = models.TextField(null=True, blank=True)
+    access_token = models.TextField()
+    refresh_token = models.TextField(null=True, blank=True)
     expires_at = models.DateTimeField()
 
     def is_token_expired(self):
