3a4c760ec93b8e2f8658eb00a2f3f2031e1993f1
hellomind <random1250@gmail.com>
2025-04-01 03:27:54 +0300

    get token

--- web/views.py
+++ web/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 import uuid
 from django.conf import settings
-from django.http import HttpResponse
-
+from django.http import HttpResponse, HttpResponseBadRequest, JsonResponse
+import requests
 
 def hh_login(request):
     print(request)
@@ -20,8 +20,30 @@ def hh_login(request):
 
 
 def hh_callback(request):
-    print(request)
-    return HttpResponse(request)
+    code = request.GET.get('code')
+    returned_state = request.GET.get('state')
+    session_state = request.session.get('oauth_state')
+
+    if returned_state != session_state:
+        return HttpResponseBadRequest("Invalid state")
+
+    # Обмениваем code на access_token
+    response = requests.post('https://hh.ru/oauth/token', data={
+        'grant_type': 'authorization_code',
+        'client_id': 'ВАШ_CLIENT_ID',
+        'client_secret': 'ВАШ_CLIENT_SECRET',
+        'code': code,
+        'redirect_uri': 'https://yourdomain.com/oauth/callback'
+    })
+
+    if response.status_code != 200:
+        return HttpResponseBadRequest("Failed to get access token")
+
+    token_data = response.json()
+    access_token = token_data.get('access_token')
+
+    # Здесь можно сохранить access_token в БД или сессию
+    return JsonResponse(token_data)
 
 
 def index(request):
